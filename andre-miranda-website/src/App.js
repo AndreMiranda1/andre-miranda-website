ffca10671e70465f7429267bd5eac49c617d8e5b
André Miranda <andremiranda@Air-de-Andre.lan>
2019-09-15 13:32:50 +0100

    Start Developing Skills page

--- andre-miranda-website/src/App.js
+++ andre-miranda-website/src/App.js
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import LandingPage from './components/LandingPage/LandingPage';
 import Bio from './components/Bio/Bio';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
       <Layout>
         <Route path="/" exact component={LandingPage} />
         <Route path="/bio" exact component={Bio} />
+        <Route path="/skills" exact component={Skills} />
       </Layout>
   );
 }
---
+++ andre-miranda-website/src/components/Skills/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Skills.module.css';
+
+const Skills = props => {
+
+  return (
+    <div>
+      <h2 className={classes.Header}>Skills</h2>
+    </div>
+  );
+}
+
+export default Skills;
