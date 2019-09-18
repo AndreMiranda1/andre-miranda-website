7592afafcfecf96975d38f993bbfbdcda79902a0
André Miranda <andremiranda@Air-de-Andre.home>
2019-09-18 22:18:47 +0100

    Develops Find Me Page

--- andre-miranda-website/src/components/FindMe/FindMe.js
+++ andre-miranda-website/src/components/FindMe/FindMe.js
@@ -10,11 +10,31 @@ const FindMe = props => {
     <div className={classes.FindMe}>
       <h2>Find Me</h2>
 
-      <div class="Social">
-        <SocialIcon url="http://www.linkedin.com/in/amlmiranda/" />
-        <SocialIcon url="mailto:amlmiranda@outlook.com" />
-        <SocialIcon url="https://github.com/AndreMiranda1/" />
+      <p>
+        If you're interested in getting in contact with me
+        you can find me on Linkedin, reach me by direct e-mail and
+        check some of my code on GitHub.
+      </p>
+
+
+      <div className={classes.SocialGroup}>
+        <div className={classes.Social}>
+          <SocialIcon url="http://www.linkedin.com/in/amlmiranda/" />
+        </div>
+        <div className={classes.Social}>
+          <SocialIcon url="mailto:amlmiranda@outlook.com" />
+        </div>
+        <div className={classes.Social}>
+          <SocialIcon url="https://github.com/AndreMiranda1/" />
+        </div>
       </div>
+
+      <p>
+        You can always try to meet me in person somewhere between Lisbon and Alentejo!
+        <br />
+        Thank you for your visit and please share your feedback.
+      </p>
+
     </div>
   );
 }
