335b3fd21881be8b284a690e3e705c7e927f405a
André Miranda <andre.miranda@cleverti.com>
2019-09-19 14:02:18 +0100

    Updates

---
+++ precache-manifest.8130641af21005e77973e2bf9079a17b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9895e7ae28ced046b83f0916f56fc983",
+    "url": "/andre-miranda-website/index.html"
+  },
+  {
+    "revision": "eca24f8feeaa2d1da610",
+    "url": "/andre-miranda-website/static/css/main.227b6f03.chunk.css"
+  },
+  {
+    "revision": "e5dc0f8fb656c49627e5",
+    "url": "/andre-miranda-website/static/js/2.427da545.chunk.js"
+  },
+  {
+    "revision": "eca24f8feeaa2d1da610",
+    "url": "/andre-miranda-website/static/js/main.abfbb59f.chunk.js"
+  },
+  {
+    "revision": "2457ac1cb4b72c7543b5",
+    "url": "/andre-miranda-website/static/js/runtime~main.f9d52656.js"
+  },
+  {
+    "revision": "88d2674aeda2132ba4d0ed155a2cafd2",
+    "url": "/andre-miranda-website/static/media/Andre_Miranda.88d2674a.jpg"
+  },
+  {
+    "revision": "618d6160bb7af9ece038114fa7890a3b",
+    "url": "/andre-miranda-website/static/media/bpi.618d6160.png"
+  },
+  {
+    "revision": "9174068b92089cbb94dfc43e7b3a5683",
+    "url": "/andre-miranda-website/static/media/cleverti.9174068b.png"
+  },
+  {
+    "revision": "57fddf35965746434ecf468c5d2ac797",
+    "url": "/andre-miranda-website/static/media/gioes.57fddf35.jpg"
+  },
+  {
+    "revision": "333b4673cabe008fc9cbd4236e71edbb",
+    "url": "/andre-miranda-website/static/media/ulisboa.333b4673.png"
+  }
+]);
