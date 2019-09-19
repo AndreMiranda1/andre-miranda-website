d3054e13385ebcfab5df0e2f3961234577142d16
André Miranda <andre.miranda@cleverti.com>
2019-09-19 15:33:32 +0100

    Fix redirects in prodcution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import LandingPage from './components/LandingPage/LandingPage';
@@ -10,17 +10,20 @@ import Background from './components/Background/Background';
 import PersonalProjects from './components/PersonalProjects/PersonalProjects';
 import FindMe from './components/FindMe/FindMe';
 
+
 function App() {
   return (
       <Layout>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/bio" exact component={Bio} />
-        <Route path="/skills" exact component={Skills} />
-        <Route path="/professional-experience" exact component={ProfessionalExperience} />
-        <Route path="/background" exact component={Background} />
-        <Route path="/personal-projects" exact component={PersonalProjects} />
-        <Route path="/find-me" exact component={FindMe} />
-        <Redirect to="/" />
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/bio" exact component={Bio} />
+          <Route path="/skills" exact component={Skills} />
+          <Route path="/professional-experience" exact component={ProfessionalExperience} />
+          <Route path="/background" exact component={Background} />
+          <Route path="/personal-projects" exact component={PersonalProjects} />
+          <Route path="/find-me" exact component={FindMe} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
       </Layout>
   );
 }
